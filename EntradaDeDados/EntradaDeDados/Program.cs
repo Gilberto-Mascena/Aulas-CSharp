a7d57be0cea9c99cdf73871ec5596c709f7b0450
Gilberto Mascena <gilbertomascena@gmail.com>
2023-08-02 00:20:09 -0300

    Adding new files

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EntradaDeDados 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            /* Entrada ou leitura de dados em CSharp é com Console.ReadLine() */
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+
+            /*
+             * Recebendo dados recortando com a função Split e armazenando 
+             * num vetor, depois atribuindo os valores recortados em variavéis. 
+             */
+            string s = Console.ReadLine();
+
+            string[] vet = s.Split(" ");
+
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+        }
+    }
+}
---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+            string nome = "Maria";
+            int idade = 32;
+            double saldo = 12.34569;
+
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+            /* .ToString("F4" formata a quantidade de casas decimais, 
+             * CultureInfo.InvariantCulture deixa o ponto como padrão de separador de decimal.)
+             */
+
+            Console.WriteLine("{0} tem {1} anos de idade e saldo de {2:F2} reais", nome, idade, saldo);
+            /* A impresão acima é definida como placeholder*/
+
+            Console.WriteLine($"{nome} tem {idade} anos de idade e saldo de {saldo:F2} reais");
+            /* A impresão acima é definida como interpolação*/
+
+            Console.WriteLine(nome + " tem " + idade + " anos de idade e saldo de " +  saldo.ToString("F2") + " reais");
+            /* A impresão acima é definida com concatenação */
+        }
+    }
+}
